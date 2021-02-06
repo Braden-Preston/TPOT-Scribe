56e26dc7c087a5fc6dea2db21730e7ff5027fec8
Braden Preston <bpfilmsinc@gmail.com>
2021-02-05 22:01:39 -0600

    terser optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path')
-const { pugWebpackAlias } = require('./plugins/pug-plugin-alias')
 
+const resolve = paths => path.resolve(process.cwd(), paths)
+
+const WebpackBar = require('webpackbar')
+const CopyPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
-const WebpackBar = require('webpackbar')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { pugWebpackAlias } = require('./plugins/pug-plugin-alias')
+
 const HtmlWebpackSkipAssetsPlugin = require('html-webpack-skip-assets-plugin')
   .HtmlWebpackSkipAssetsPlugin
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
-const resolve = paths => path.resolve(process.cwd(), paths)
+//
+// Webpack Production Bundle Configuration
+//
 
 /** @type {import("webpack").Configuration } */
 module.exports = {
@@ -21,11 +28,9 @@ module.exports = {
     index: ['./src/index.js', './src/index.css']
   },
   output: {
-    path: resolve('build')
-    // filename: data => {
-    //   // console.log('pathData', data)
-    //   return data.chunk.name === 'index' ? '[name].js' : 'js/[name].js'
-    // }
+    path: resolve('build'),
+    filename: data =>
+      data.chunk.name === 'index' ? '[name].js' : 'js/[name].js'
   },
   resolve: {
     extensions: ['.js', 'ts', 'pug'],
@@ -54,7 +59,84 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new WebpackBar(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: data =>
+        data.chunk.name === 'index' ? '[name].css' : 'css/[name].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/*.pug']
+          }
+        }
+      ]
+    }),
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true
+    }),
+    new WebpackManifestPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: 'index.html',
+      template: './public/index.pug',
+      minify: {
+        html5: true,
+        decodeEntities: true,
+        removeComments: true,
+        useShortDoctype: true,
+        keepClosingSlash: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true
+      },
+      excludeAssets: ['index.js', 'index.css']
+    }),
+    new HtmlWebpackSkipAssetsPlugin()
+  ],
   optimization: {
+    // emitOnErrors: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: false,
+        terserOptions: {
+          ecma: '2018',
+          format: {
+            semicolons: false,
+            comments: false
+          },
+          compress: {
+            passes: 10,
+            booleans_as_integers: true,
+            drop_console: true,
+            keep_fargs: false,
+            arguments: true,
+            join_vars: true,
+            typeofs: true,
+            module: true,
+            loops: true,
+            unused: true,
+            unsafe: true,
+            unsafe_math: true,
+            unsafe_comps: true,
+            unsafe_proto: true,
+            unsafe_regexp: true,
+            unsafe_symbols: true,
+            unsafe_methods: true,
+            unsafe_Function: true,
+            unsafe_undefined: true
+          }
+        }
+      })
+    ],
     splitChunks: {
       cacheGroups: {
         models: {
@@ -83,28 +165,5 @@ module.exports = {
         // },
       }
     }
-  },
-  plugins: [
-    new WebpackBar(),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      title: 'Snowpack App',
-      filename: 'index.html',
-      template: './public/index.pug',
-      minify: {
-        removeComments: true
-      }
-    }),
-    new HtmlWebpackSkipAssetsPlugin({
-      excludeAssets: ['index.js', 'index.css']
-    }),
-    new BundleAnalyzerPlugin({
-      generateStatsFile: true
-    }),
-    new WebpackManifestPlugin()
-  ]
+  }
 }
